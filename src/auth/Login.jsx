c5f15ac6a5bfb57bb416a9db788d32a47da20f3a
bcarcamo247 <bcarcamo247@gmail.com>
2026-06-25 10:54:47 -0400

    add login forms

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router";
-
 import { useAuth } from "./AuthContext";
 
-/** A form that allows users to log into an existing account. */
+/** A form that allows users to log into an existing account.
+ Receives data from auth context file. Handle form added logging in.
+ Get username and password from form. Handle login errors.
+ */
 export default function Login() {
   const { login } = useAuth();
   const navigate = useNavigate();
@@ -20,7 +22,7 @@ export default function Login() {
       setError(e.message);
     }
   };
-
+/*Return login form and errors to display on screen */
   return (
     <>
       <h1>Log in to your account</h1>
