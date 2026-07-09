e2863256c8f1d1c8554dc41d5e883e157c195c48
AOrnstein <ornstein.adam@gmail.com>
2026-07-09 11:31:18 -0600

    use card compo0netn in compendium page

--- src/pages/Compendium.jsx
+++ src/pages/Compendium.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useCards } from "../contexts/useCards";
 import "../styles/pages/Compendium.css";
+import CardTile from "../components/CardTile";
 
 export default function Compendium() {
   const { cards } = useCards();
@@ -34,106 +35,9 @@ export default function Compendium() {
 
       <div className="card-grid">
         {filteredItems.map((card) => (
-          <CardDetailsPopover key={card.id} card={card}>
-            <img
-              className="card-art"
-              src={card.card_front_url}
-              alt={card.name}
-            />
-            <div className="card-info">
-              <h2>{card.name}</h2>
-            </div>
-          </CardDetailsPopover>
+          <CardTile key={card.id} card={card} label={card.name} />
         ))}
       </div>
     </section>
-  );
-}
-
-/** Popover for card details */
-export function CardDetailsPopover({ card, children }) {
-  return (
-    <>
-      <button className="card-tile" popovertarget={`card-${card.id}-dialog`}>
-        {children}
-      </button>
-
-      {/* Dialog Element */}
-      <dialog
-        id={`card-${card.id}-dialog`}
-        className="card-popover"
-        popover="auto"
-      >
-        <div className="card-info">
-          <h2>{card.name}</h2>
-          <p>{card.content.description}</p>
-          <CardOrientationDetails
-            card={card}
-            label="Upright"
-            orientation="upright"
-          />
-          <CardOrientationDetails
-            card={card}
-            label="Reversed"
-            orientation="reversed"
-          />
-        </div>
-        <button
-          className="popover-close"
-          popovertarget={`card-${card.id}-dialog`}
-          popovertargetaction="hide"
-          autoFocus
-        >
-          Close
-        </button>
-      </dialog>
-    </>
-  );
-}
-
-/**
- * Component for card details for an orientation
- * @param {{card: Object, label: string, orientation:"upright"|"reversed" }}}
- * @property {Object} card - card data
- * @property {string} label - orentation details section label
- * @property {"upright"|"reversed"} orientation - orientation type
- */
-function CardOrientationDetails({ card, label, orientation }) {
-  return (
-    <>
-      <h3>{label}</h3>
-      <ul>
-        <li>
-          <p>
-            <span className="card-property-label">Person - </span>
-            <span>{card.content[orientation].person}</span>
-          </p>
-        </li>
-        <li>
-          <p>
-            <span className="card-property-label">Creature or Trap - </span>
-            <span>{card.content[orientation].creature_or_trap}</span>
-          </p>
-        </li>
-        <li>
-          <p>
-            <span className="card-property-label">Place - </span>
-            <span>{card.content[orientation].place}</span>
-          </p>
-        </li>
-        <li>
-          <p>
-            <span className="card-property-label">Treasure - </span>
-            <span>{card.content[orientation].treasure}</span>
-          </p>
-        </li>
-        <li>
-          <p>
-            <span className="card-property-label">Situation - </span>
-            <span>{card.content[orientation].situation}</span>
-          </p>
-        </li>
-      </ul>
-    </>
   );
 }
